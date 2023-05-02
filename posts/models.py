f4490f3709fb59e200fdbdc1efdc4b6683344355
TheJessDev <1jessicalmorrison@gmail.com>
2023-05-02 10:52:21 -0600

    First

---
+++ posts/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.urls import reverse
+# create models
+
+class Post(models.Model):                           # example of inheritance
+    title = models.CharField(max_length=128)         # multiple examples of composits
+    subtitles = models.CharField(max_length=256)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("post_detail", args=[self.id])
+
+
